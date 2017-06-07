f31844584427691019083231b9637b97ace7838c
Boyko Alexey <ket4yiit@gmail.com>
2017-06-07 14:27:07 +0300

    Run nginx by consul exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 consul-template \
     -template "$NGINX_TMPL_PATH:$NGINX_CONF_PATH:nginx -s reload" \
-    -consul $CONSUL_SERVER && \
-nginx -g "daemon off;" 
+    -consul $CONSUL_SERVER \
+    -exec 'nginx -g "daemon off;"' 
