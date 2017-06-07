dac60345dd21105beab11472415985dcd1c3b657
Boyko Alexey <ket4yiit@gmail.com>
2017-06-07 13:35:05 +0300

    Fix entrypoint commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
+nginx -g "daemon off;" && \
 consul-template \
     -template "$NGINX_TMPL_PATH:$NGINX_CONF_PATH:nginx -s reload" \
     -consul $CONSUL_SERVER \
-    && \
-nginx -g "daemon off"
