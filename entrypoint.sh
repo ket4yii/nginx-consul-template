3d279e6143a4ffcde41d4c89cc1a333a1cb53c97
Boyko Alexey <ket4yiit@gmail.com>
2017-06-07 14:03:40 +0300

    Delete daemonise of consul-template

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
-#!/bin/sh      Do not run the process as a daemon
+#!/bin/sh
 
 nginx -g "daemon off;" && \
 consul-template \
-    -once \
     -template "$NGINX_TMPL_PATH:$NGINX_CONF_PATH:nginx -s reload" \
     -consul $CONSUL_SERVER
